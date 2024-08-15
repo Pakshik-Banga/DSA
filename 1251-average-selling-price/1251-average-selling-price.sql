13be6ac98572706ce6c2198437c519e2d846ac68
Pakshik-Banga <114055506+Pakshik-Banga@users.noreply.github.com>
2024-08-16 02:19:12 +0530

    Time: 667 ms (72.23%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 select p.product_id , 
-case when round(sum(u.units * p.price) / sum(u.units), 2) is null then 0.00 else round(sum(u.units * p.price) / sum(u.units), 2) end  as average_price from prices as p
+coalesce(round(sum(u.units * p.price) / sum(u.units), 2),0)  as average_price from prices as p
 left join unitsSold as u
 on p.product_id = u.product_id and (u.purchase_date between p.start_date and p.end_date)
 group by p.product_id
